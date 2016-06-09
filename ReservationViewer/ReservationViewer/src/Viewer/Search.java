227beb226155f4b3262e5149f6f593c93e5f613e
Jasonvbowen <jasonvbowen@gmail.com>
2016-06-08 23:18:57 -0700

    made it all work

--- ReservationViewer/ReservationViewer/src/Viewer/Search.java
+++ ReservationViewer/ReservationViewer/src/Viewer/Search.java
@@ -1,78 +1,86 @@
 package Viewer;
 
-import java.util.Scanner;
-import javax.swing.JOptionPane;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
- * File Name: Search.java
- * Description: This class will do a binary search on the reservation array
- * which includes all the customer's from the database file and return a value
- * if the item was found or if it wasn't found return a error message.
- * Date: 6/8/2016
- * Platform: Windows 8, jdk 1.8.0_66, NetBeans 8.1
+ * File Name: Search.java Description: This class will do a binary search on the
+ * reservation array which includes all the customer's from the database file
+ * and return a value if the item was found or if it wasn't found return a error
+ * message. Date: 6/8/2016 Platform: Windows 8, jdk 1.8.0_66, NetBeans 8.1
+ *
  * @author Branavan Nagendiram , Luka Gajic , Jason Bowen
  */
-public class Search implements Comparable
-{
-    public static int NOT_THERE = -1;
-    public static int EQUAL = 0;
+public class Search {
+
+    private static ArrayList<Comparable> searchList;
+
+    public static ArrayList<Comparable> binarySearch(Comparable[] arr, Comparable searchValue) {
+        ArrayList<Comparable> comp = new ArrayList<>();
+        comp.addAll(Arrays.asList(arr));
+        searchList = new ArrayList<>();
+        binarySearch(comp, searchValue, 0, comp.size());
+        return searchList;
+    }
+
     /**
-     * Description: This is a binary search method which will search through
-     * the array of customer reservations and look for the customer that the 
-     * user has searched for, if that name isn't there it will return a -1.
+     * Description: This is a binary search method which will search through the
+     * array of customer reservations and look for the customer that the user
+     * has searched for, if that name isn't there it will return a -1.
+     *
      * @param arr - array of customer's
      * @param searchValue - string that user searched for
      * @param low - low index of array
      * @param high - high index of array
      * @return integer value of the array that the customer name is in
      */
-    
-    public static int BinarySearch(Comparable[] arr, Comparable searchValue
-     , int low , int high)
-    {
-        if(high <= low)
-        {
-            return NOT_THERE;
+    private static void binarySearch(ArrayList<Comparable> arr, Comparable searchValue, int low, int high) {
+        if (high <= low) {
+            return;
         }
-        int middleValue = (high + low) / 2;
-        if(arr[middleValue].equals(searchValue))
-        {
-            return middleValue;
+        int mid = (high + low) / 2;
+        if (arr.get(mid).equals(searchValue)) {
+            searchList.add(arr.remove(mid));
+            binarySearch(arr, searchValue, 0, arr.size());
+        } else if (arr.get(mid).compareTo(searchValue) > 0) {
+            binarySearch(arr, searchValue, low, mid);
+        } else {
+            binarySearch(arr, searchValue, mid + 1, high);
         }
-        else if(arr[middleValue].compareTo(searchValue) > 0 )
-        {
-            return BinarySearch(arr, searchValue, low , middleValue);
-        }
-        else
-            return BinarySearch(arr, searchValue, middleValue + 1 , high);
     }
-    
- 
 
-    
-    public int compareTo(Reservation obj)
-    {
-    try
-    {
-        int compare = test.getCustomerName().compareTo(obj.getString());
-        if(compare != EQUAL)
-        {
-            return compare;
+    public static ArrayList<Comparable> binarySearchFirstLetter(Comparable[] arr, char searchedLetter) {
+        ArrayList<Comparable> comp = new ArrayList<>();
+        comp.addAll(Arrays.asList(arr));
+        searchList = new ArrayList<>();
+        binarySearchFirstLetter(comp, Character.toLowerCase(searchedLetter), 0, comp.size());
+        return searchList;
+    }
+
+    /**
+     * Description: This is a binary search method which will search through the
+     * array of customer reservations and look for the customer that the user
+     * has searched for, if that name isn't there it will return a -1.
+     *
+     * @param arr - array of customer's
+     * @param searchValue - string that user searched for
+     * @param low - low index of array
+     * @param high - high index of array
+     * @return integer value of the array that the customer name is in
+     */
+    private static void binarySearchFirstLetter(ArrayList<Comparable> arr, char searchedLetter, int low, int high) {
+        if (high <= low) {
+            return;
+        }
+        int mid = (high + low) / 2;
+        char firstLetter = Character.toLowerCase(arr.get(mid).toString().charAt(0));
+        if (firstLetter == searchedLetter) {
+            searchList.add(arr.remove(mid));
+            binarySearchFirstLetter(arr, searchedLetter, 0, arr.size());
+        } else if (firstLetter > searchedLetter) {
+            binarySearchFirstLetter(arr, searchedLetter, low, mid);
+        } else {
+            binarySearchFirstLetter(arr, searchedLetter, mid + 1, high);
         }
     }
-    catch(ClassCastException exp)
-    {
-        JOptionPane.showMessageDialog(null, "Class is Incorrect", 
-                       "Class Error", JOptionPane.ERROR_MESSAGE);
-    }
-        return EQUAL;
-    }
-    ViewerGUI test = new ViewerGUI();
-
-    @Override
-    public int compareTo(Object o) {
-        return compareTo((Reservation) o);
-    }
-    
 }
- 
